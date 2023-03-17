fa26c6d3e4dc3c6a582a76185c0d81345d8d5c1f
Yogesh Yadav <59149010+yogeshft@users.noreply.github.com>
2023-03-17 12:01:49 +0530

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,31 @@
+(function() {
+
+    const screen = document.querySelector('.screen');    
+    const buttons = document.querySelectorAll('.click');
+    const clear = document.querySelector('.btn-clear');
+    const equal = document.querySelector('.btn-equal');
+    
+    buttons.forEach(function(button){
+    button.addEventListener('click', function(e){
+        let value = e.target.dataset.num;
+        screen.value +=value;
+        
+       
+    })
+});   
+    equal.addEventListener('click', function(e) {
+
+        if(screen.value===''){
+            screen.value = '';
+        } else {
+            let answer = eval(screen.value);
+           
+            screen.value = answer;
+        }
+    });
+    
+    clear.addEventListener('click',function(e){
+        screen.value='';
+    });
+})();
+
